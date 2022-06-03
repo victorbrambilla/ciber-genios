df36395e7bbc1cc800581ecc7c72b49ca4dce481
victor brambilla <88890771+victorbrambilla@users.noreply.github.com>
2022-06-02 23:31:34 -0400

    Fix(Clear code): Retirando código não usado

--- src/pages/table/index.tsx
+++ src/pages/table/index.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from 'next';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { Header } from '../../components/header';
 import Container from './styles';
 
@@ -7,26 +7,10 @@ import { TableComponent } from '../../components/tableComponent';
 import { ModalComponent } from '../../components/modal';
 
 const Table: NextPage = () => {
-  const loaderRef = useRef(null);
-
-  const onScroll = () => {
-    console.log('');
-    if (loaderRef.current) {
-      const { scrollTop, scrollHeight, clientHeight } = loaderRef.current;
-      if (scrollTop + clientHeight === scrollHeight) {
-        //makingsettimeout
-        console.log('cheogu');
-      }
-    }
-  };
-
-  useEffect(() => {
-    onScroll();
-  });
   return (
     <>
       <Header />
-      <Container ref={loaderRef}>
+      <Container>
         <div className='header'>
           <h2>Lista</h2>
           <ModalComponent type='Create' />
